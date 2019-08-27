5599ed3df54cf557a3b5070c7e753ef9837fb105
romiinavega <romiivega3005@gmail.com>
2019-08-27 13:00:08 -0700

    cambioc

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const buttons = document.querySelectorAll('button');
 const playnote = event => {
-    console.log (event.target.dataset.note);
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note}`;
+    console.log(audioId);
+    const audio = document.getElementById (audioId);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+    
 }
 
 buttons.forEach(
-button => button.addEventListener('click' , alert ('hola crayola') )
+button => button.addEventListener('click', playnote )
 );
